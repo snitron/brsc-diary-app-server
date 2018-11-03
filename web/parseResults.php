f9855e7feac10a294bc97949f49089a8709b12c0
snitron <nvar2@yandex.ru>
2018-11-03 23:49:14 +0300

    test parse

--- web/parseResults.php
+++ web/parseResults.php
@@ -79,7 +79,6 @@ if(count($tables) == 2){
     $length = count($results);
     for($i = $length + 1; $i < count($trS) + $length + 1; $i++) {
         $tr = HtmlDomParser::str_get_html($trS[$i - $length]);
-        echo "<plaintext>". $tr . "</plaintext>";
         $tdS = $tr->find("td");
         $result = new Result();
         for($j = 0; $j < count($tdS); $j++){
