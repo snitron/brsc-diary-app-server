87286ddba72f8caf4b82e0d6da8ac018bfb970b0
snitron <nvar2@yandex.ru>
2018-11-04 19:10:29 +0300

    added getName functions

--- web/getName.php
+++ web/getName.php
@@ -27,7 +27,6 @@ $html = new Document($snoopy->results);
 $result = new Person();
 
 $result->name = parseName($html->find("tr")[0]->find("th")[0]->text());
-echo $html->find("span.pull-left")[1]->html();
 $result->img = $html->find("span.pull-left")[1]->first("img")->attr("src");
 
 echo json_encode($result);
