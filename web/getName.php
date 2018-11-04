9d5ea517b9f7c01f1730823b58b7cd7a9b902c8c
snitron <nvar2@yandex.ru>
2018-11-04 18:24:40 +0300

    added getName functions

--- web/getName.php
+++ web/getName.php
@@ -5,6 +5,7 @@ use DiDom\Document;
 
 $login = filter_input(INPUT_GET, "login", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_GET, "password", FILTER_SANITIZE_STRING);
+$userID = filter_input(INPUT_GET, "userID", FILTER_SANITIZE_STRING);
 
 $snoopy = new Snoopy();
 
@@ -16,7 +17,12 @@ $snoopy->maxredirs = 2;
 $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
 $snoopy->results;
 
-$snoopy->submit("https://edu.brsc.ru/User/Diary");
+$snoopy->submit("https://edu.brsc.ru/user/diary/diaryresult?UserId=" . $userID);
 $html = new Document($snoopy->results);
 
-echo $html->html();
+echo parseName($html->find("tr")[0]->find("th")[0]->text());
+
+
+function parseName($string){
+    return substr($string, 0, strpos($string, ','));
+}
