4193cc9945d8401ba5aabb1f4402aa09df184d0b
Nikita <25131222+snitron@users.noreply.github.com>
2019-03-15 20:38:11 +0300

    Update getJWT.php

--- web/getJWT.php
+++ web/getJWT.php
@@ -13,9 +13,10 @@ $post_array['Login'] = $login;
 $post_array['Password'] = $password;
 $snoopy->maxredirs = 2;
 $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
+$snoopy->submit("https://edu.brsc.ru/User/Diary");
 for($i = 0; $i < $snoopy->headers; $i++){
     echo $snoopy->headers[$i];
 }
 
-       
-     
+
+
