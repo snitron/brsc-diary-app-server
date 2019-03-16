243117a584f2b8dcc059d45c9330951253ac6d91
Nikita <25131222+snitron@users.noreply.github.com>
2019-03-16 14:34:16 +0300

    Update getPageByJWT.php

--- web/getPageByJWT.php
+++ web/getPageByJWT.php
@@ -1,17 +1,12 @@
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjQ5NDEyOSIsInJvbGUiOiI4LERlcGFydG1lbnQsLDUyLDQ3OSw2MjI4NywiLCJuYmYiOjE1NTI2NzY0MDAsImV4cCI6MTU1MzI4MTIwMCwiaWF0IjoxNTUyNzM1MTYyLCJpc3MiOiIxMC4zLjIuMzAiLCJhdWQiOiJpdC5icnNjLnJ1In0.42ovgYR9hVKhNqvTFk9oNSwwhStbXvi3dWEElMZtJSI
-
 <?php
 require __DIR__ . "/../vendor/autoload.php";
-
-
 use Snoopy\Snoopy;
 
+$snoopy = new Snoopy();
+$snoopy->maxredirs = 2;
+$snoopy->rawheaders['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjQ5NDEyOSIsInJvbGUiOiI4LERlcGFydG1lbnQsLDUyLDQ3OSw2MjI4NywiLCJuYmYiOjE1NTI2NzY0MDAsImV4cCI6MTU1MzI4MTIwMCwiaWF0IjoxNTUyNzM1MTYyLCJpc3MiOiIxMC4zLjIuMzAiLCJhdWQiOiJpdC5icnNjLnJ1In0.42ovgYR9hVKhNqvTFk9oNSwwhStbXvi3dWEElMZtJSI";
+$snoopy->cookies['JWToken'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjQ5NDEyOSIsInJvbGUiOiI4LERlcGFydG1lbnQsLDUyLDQ3OSw2MjI4NywiLCJuYmYiOjE1NTI2NzY0MDAsImV4cCI6MTU1MzI4MTIwMCwiaWF0IjoxNTUyNzM1MTYyLCJpc3MiOiIxMC4zLjIuMzAiLCJhdWQiOiJpdC5icnNjLnJ1In0.42ovgYR9hVKhNqvTFk9oNSwwhStbXvi3dWEElMZtJSI";
+$snoopy->submit("https://edu.brsc.ru/User/Diary");
 
-$headers = getallheaders();
-
-        $snoopy = new Snoopy();
-       
-        $snoopy->maxredirs = 2;
-        $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
-        $snoopy->submit("https://edu.brsc.ru/User/Diary");
+echo $snoopy->getResults();
        
