413b5bb1692e8af62fd7e2dd8b5d24a4ff694f10
Nikita <25131222+snitron@users.noreply.github.com>
2019-03-16 17:40:31 +0300

    Update getId.php

--- web/getId.php
+++ web/getId.php
@@ -2,83 +2,58 @@
 require __DIR__ . "/../vendor/autoload.php";
 require "User.php";
 
-use Snoopy\Snoopy;
 use \DiDom\Document;
 
+
 $headers = getallheaders();
-if ($headers['User-Agent'] == 'Nitron Apps BRSC Diary Http Connector') {
+//if ($headers['User-Agent'] == 'Nitron Apps BRSC Diary Http Connector') {
     $login = filter_input(INPUT_GET, "login", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_GET, "password", FILTER_SANITIZE_STRING);
     $version = filter_input(INPUT_GET, "version", FILTER_SANITIZE_STRING);
 
+    $driver = new \Behat\Mink\Driver\GoutteDriver();
+    $session = new \Behat\Mink\Session($driver);
+
+    $session->start();
+    $session->visit("https://edu.brsc.ru/Logon/Index");
+    $btn = $session->getPage()->findButton("Войти");
+    $loginEdit = $session->getPage()->findField("Login");
+    $loginEdit->setValue($login);
+    $passwordEdit = $session->getPage()->findField("Password");
+    $passwordEdit->setValue($password);
+    $btn->click();
+    $session->visit("https://edu.brsc.ru/privateoffice");
+    $html = new Document($session->getPage()->getHtml());
+
     if ($version != null) { //for eldery version support. delete in the future
         //  session_start();
 
-        $snoopy = new Snoopy();
 
-        $post_array = array();
-        $post_array['Login'] = $login;
-        $post_array['Password'] = $password;
-
-        $snoopy->maxredirs = 2;
-        $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
-        $snoopy->submit("https://edu.brsc.ru/User/Diary");
-
-        $html = new Document($snoopy->results);
+        $html = new Document($session->getPage()->getHtml());
 
         $user = new User();
-        $check_login = $html->find("tr");
+        $check_login = $html->find("a.btn.btn-sm.btn-primary[=Дневник]");
         if (count($check_login) != 0) {
-            $check = $html->find("span.dropdown");
-          
-         
-            if (count($check) != 0) {
-                  $child_ids = $html->find("ul.dropdown-menu")[0]->find("li");
 
-            $user->child_ids[0] = parseId($html->find("a.h5")[0]->getAttribute("href"));
-                for ($i = 1; $i < count($child_ids) + 1; $i++)
-                    $user->child_ids[$i] = parseId($child_ids[$i - 1]->find("a")[0]->getAttribute("href"));
+            for ($i = 0; $i < count($check_login); $i++)
+                $user->child_ids[$i] = parseId($check_login[$i]->getAttribute("href"));
 
-                $user->id = null;
-                $user->sess_index = "user_index" . $user->id;
-                $user->parent_id = $user->parent_id[0];
-            } else {
-                $user->child_ids = null;
-                $user->id = parseId($html->find("a.h5")[0]->getAttribute("href"));
-                $user->sess_index = "user_index" . $user->id;
-                $user->parent_id = null;
-            }
-
-            $user->sess_id = "PHPSESSID=" . session_id();
-            /*
-                    $sess_data = array();
-
-                    $sess_data['login'] = $login;
-                    $sess_data['password'] = $password;
-                    $_SESSION[$id] = $sess_data;*/
-
-        } else
-            $user = null;
+            $user->id = null;
+            $user->sess_index = "user_index" . $user->id;
+            $user->parent_id = $html->find("p.col-lg-7.col-md-8.col-sm-8.col-6")[0]->text();
+        } else {
+            $user->child_ids = null;
+            $user->id = $html->find("p.col-lg-7.col-md-8.col-sm-8.col-6")[0]->text();
+            $user->sess_index = "user_index" . $user->id;
+            $user->parent_id = null;
+        }
 
 
         echo json_encode($user);
     } else {
-        $snoopy = new Snoopy();
-
-        $post_array = array();
-        $post_array['Login'] = $login;
-        $post_array['Password'] = $password;
-
-        $snoopy->maxredirs = 2;
-        $snoopy->submit("https://edu.brsc.ru/Logon/Index", $post_array);
-        $snoopy->results;
-
-        $snoopy->submit("https://edu.brsc.ru/User/Diary");
-        $html = new Document($snoopy->results);
-
-        echo parseId($html->find("a.h5")[0]->getAttribute("href"));
+        echo  $html->find("p.col-lg-7.col-md-8.col-sm-8.col-6")[0]->text();
     }
-}
+//}
 
 function parseId($string)
 {
